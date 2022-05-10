abd6c9d6681c96b42aacd168a35402d584cb073a
marirazak <mariamrazak01@gmail.com>
2022-05-10 10:57:03 -0700

    stuff

--- 179_Tune_Tower_Defense/Assets/Board.cs
+++ 179_Tune_Tower_Defense/Assets/Board.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    List <float> NoteOrder = new List <float> () {1, 3, 4, 6, 1, 2, 5, 5, 6, 2, 4, 3, 5, 1, 5, 2, 4};
+    List <float> NoteOrder = new List <float> () {1, 3, 4, 6, 1, 2, 5, 5, 6, 2, 4, 3, 5, 1, 5, 2, 4, 5, 1, 3, 6, 5, 4, 1, 2};
     //List <float> NoteOrder = new List <float> () {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     // Start is called before the first frame update
     public Transform noteObj;
