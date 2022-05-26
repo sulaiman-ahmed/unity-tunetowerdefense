938024b21e6e3a1123e41f8230cc6cd1c57dcf42
marirazak <mariamrazak01@gmail.com>
2022-05-25 18:20:24 -0700

    note hit

--- 179_Tune_Tower_Defense/Assets/RhythmBoard/hitNote.cs
+++ 179_Tune_Tower_Defense/Assets/RhythmBoard/hitNote.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class hitNote : MonoBehaviour
 {
+    public Transform hitBurst;
     
     // Start is called before the first frame update
     void Start()
@@ -28,26 +29,32 @@ public class hitNote : MonoBehaviour
                     if(vKey == KeyCode.Q && other.gameObject.name == "hitCollider1") { 
                         Debug.Log("success Q");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                     else if(vKey == KeyCode.W && other.gameObject.name == "hitCollider2") { 
                         Debug.Log("success W");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                     else if(vKey == KeyCode.E && other.gameObject.name == "hitCollider3") { 
                         Debug.Log("success E");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                     else if(vKey == KeyCode.A && other.gameObject.name == "hitCollider4") { 
                         Debug.Log("success A");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                     else if(vKey == KeyCode.S && other.gameObject.name == "hitCollider5") { 
                         Debug.Log("success S");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                     else if(vKey == KeyCode.D && other.gameObject.name == "hitCollider6") { 
                         Debug.Log("success D");
                         Destroy(gameObject);
+                        Instantiate (hitBurst, transform.position, hitBurst.rotation);
                     }
                    
                 }
